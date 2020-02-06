3cda5568e3291c4996c5d2321afe1aa03a0f5c14
Danilo <danilo.campagnolo.93@gmail.com>
2020-02-06 12:27:02 +0100

    printFilmsFound

--- js/script.js
+++ js/script.js
@@ -11,15 +11,29 @@ $(document).ready(function() {
         language: "it-IT"
       },
       success: function(data) {
-        console.log(data.results);
+        var filmsFound = data.results;
+        printFilmsFound(filmsFound);
       },
       error: function() {
 
       }
     });
   });
-
-
-
-
 });
+
+function printFilmsFound(array) {
+  $(".films").text("");
+  for (var i = 0; i < array.length; i++) {
+    array[i]
+    // handlebars
+    var source = document.getElementById("entry-template").innerHTML;
+    var template = Handlebars.compile(source);
+    var context = {
+      original_title: array[i].original_title,
+      original_language: array[i].original_language,
+      vote_average: array[i].vote_average
+    };
+    var html = template(context);
+    $(".films").append(html);
+  }
+}
